11350e65d6490cfc505e80dcf0fff5ba284019ce
Yu-hanCheng <sarahcheng1231@gmail.com>
2018-06-07 11:00:15 +0800

    0607

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,41 @@
+// Home.vue
+
+<template>
+    <div>
+        <p>Home page</p>
+        <p>Random number from backend: {{ randomNumber }}</p>
+        <button @click="getRandom">New random number</button>
+	</div>
+</template>
+
+<script>
+import Math from 'math'
+import axios from 'axios'
+export default{
+
+	data(){
+		return {
+			randomNumber:0
+		}
+	},
+	methods: {
+		getRandom (){
+		this.randomNumber = this.getRandomFromBackend()
+		},
+		getRandomFromBackend (){
+			const path = 'http://localhost:5000/api/random'
+			axios.get(path)
+			.then(response =>{
+				this.randomNumber = response.data.randomNumber
+				console.log(response)
+			})
+			.catch(error => {
+				console.log(error.response)
+			})
+		}
+	},
+	created () {
+		this.getRandom()
+	}
+}
+</script>
